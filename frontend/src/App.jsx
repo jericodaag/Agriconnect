eee677ea4af33498bce376ecc7c33f87a7861371
jericodaag <daageco@gmail.com>
2024-09-15 12:57:53 +0800

    User UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Shops from './pages/Shops';
+import Card from './pages/Card';
+import Shipping from './pages/Shipping';
+import Details from './pages/Details';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/shops' element={<Shops/>} />
+      <Route path='/card' element={<Card/>} />
+      <Route path='/shipping' element={<Shipping/>} />
+      <Route path='/product/details/:slug' element={<Details/>} /> 
 
     </Routes>
     
