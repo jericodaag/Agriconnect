04964068729fcfc36181d76442f9b999b080ff08
jericodaag <daageco@gmail.com>
2024-09-16 13:05:53 +0800

    User Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,13 @@ import { get_category } from './store/reducers/homeReducer';
 import { useDispatch} from 'react-redux';
 import CategoryShop from './pages/CategoryShop';
 import SearchProducts from './pages/SearchProducts';
+import Payment from './pages/Payment';
+import Dashboard from './pages/Dashboard';
+import ProtectUser from './utils/ProtectUser';
+import Index from './components/dashboard/Index';
+import Orders from './components/dashboard/Orders';
+import ChangePassword from './components/dashboard/ChangePassword';
+import Wishlist from './components/dashboard/Wishlist';
 
 function App() {
   const dispatch = useDispatch()
@@ -30,9 +37,25 @@ function App() {
       <Route path='/shops' element={<Shops/>} />
       <Route path='/card' element={<Card/>} />
       <Route path='/shipping' element={<Shipping/>} />
+      <Route path='/payment' element={<Payment/>} />
       <Route path='/products?' element={<CategoryShop/>} />
       <Route path='/products/search?' element={<SearchProducts/>} />
       <Route path='/product/details/:slug' element={<Details/>} /> 
+
+      <Route path='/dashboard' element={<ProtectUser/>} >
+      <Route path='' element={<Dashboard/>} >        
+      <Route path='' element={<Index/>} />
+      <Route path='my-orders' element={<Orders/>} /> 
+      <Route path='change-password' element={<ChangePassword/>} /> 
+      <Route path='my-wishlist' element={<Wishlist/>} /> 
+       
+       </Route> 
+      </Route>
+      
+       
+
+
+
 
     </Routes>
     
--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -1,10 +1,14 @@
 import authReducer from "./reducers/authReducer";
 import cardReducer from "./reducers/cardReducer";
+import dashboardReducer from "./reducers/dashboardReducer";
 import homeReducer from "./reducers/homeReducer";
+import orderReducer from "./reducers/orderReducer";
 
 const rootReducer = {
     home: homeReducer,
     auth: authReducer,
-    card: cardReducer
+    card: cardReducer,
+    order: orderReducer,
+    dashboard: dashboardReducer
 }
 export default rootReducer;
